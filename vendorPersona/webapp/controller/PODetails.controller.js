7d96fd39bf1ded09bbd7217c8fb7ff3a3cffd582
Venkatesh Hulekal <Venkatesh.Hulekal@extentia.com>
2021-03-22 12:13:40 +0000

    Final chnages to feature List PO, filter/sort, Details of PO

--- vendorPersona/webapp/controller/PODetails.controller.js
+++ vendorPersona/webapp/controller/PODetails.controller.js
@@ -119,48 +119,6 @@ sap.ui.define([
             oEvent.getSource().setBusy(false);
         },
 
-        onParentItemCreatePress: function (oEvent) {
-            if (!this._oCreateParentItemDialog) {
-                this._oCreateDialog = sap.ui.xmlfragment("com.agel.mmts.vendorPersona.view.fragments.PODetails.CreateParentItem", this);
-                this.getView().addDependent(this._oCreateDialog);
-            }
-            this._oCreateDialog.open();
-        },
-
-        closeDialog: function (oEvent) {
-            this._oCreateDialog.close();
-        },
-
-        onSaveParentLineItemPress: function (oEvent) {
-            var oTable = this.getView().byId("idParentLineItemsTable"),
-                oBinding = oTable.getBinding("items"),
-                aInputData = this.getViewModel("parentItemCreationModel").getData(),
-                oViewContext = this.getView().getBindingContext().getObject(),
-
-                // Create a new entry through the table's list binding
-                oContext = oBinding.create({
-                    "parent_line_item_id": (Math.floor(Math.random() * (999999999999 - 900000000000 + 1)) + 900000000000).toString(),
-                    "description": aInputData.description,
-                    "po_number": oViewContext.po_number,
-                    "material_code": aInputData.material_code,
-                    "qty": parseInt(aInputData.qty),
-                    "purchase_order_ID": oViewContext.ID
-                });
-
-            console.log(oContext);
-
-
-            oContext.created().then(function () {
-                debugger;
-                //var oEntry = this.getObject();
-                //sap.m.MessageBox.success("New entry created with name " + oEntry.name + " and email " + oEntry.email);
-            }.bind(oContext), function (error) {
-                debugger;
-                //sap.m.MessageBox.success("Error Creating Entries!!");
-            }.bind(oContext));
-            this.closeDialog();
-        },
-
         onExportParentItemsExportPress: function (oEvent) {
             var aCols, oRowBinding, oSettings, oSheet, oTable;
 
@@ -212,79 +170,6 @@ sap.ui.define([
                 .finally(function () {
                     oSheet.destroy();
                 });
-        },
-        
-        onConfirmPO: function (oEvent) {
-            //initialize the action
-            var that = this,
-                oViewContext = this.getView().getBindingContext().getObject(),
-                oBindingObject = oEvent.getSource().getObjectBinding();
-
-            //set the parameters
-            oBindingObject.getParameterContext().setProperty("status", "CONFIRMED");
-            oBindingObject.getParameterContext().setProperty("po_uuid", oViewContext.ID);
-
-            //execute the action
-            oBindingObject.execute().then(
-                function () {
-                    MessageToast.show("PO confirmed!");
-                    that.getView().getModel().refresh();
-                },
-                function (oError) {
-                    sap.m.MessageBox.alert(oError.message, {
-                        title: "Error"
-                    });
-                }
-            );
-        },
-
-        fileUploaonBOQFileSelectedForUploadderChange: function (oEvent) {
-            // keep a reference of the uploaded file
-            var that = this;
-            var oFiles = oEvent.getParameters().files;
-            this._getImageData(URL.createObjectURL(oFiles[0]), function (base64) {
-                that._addData(base64);
-            });
-        },
-
-        _getImageData: function (url, callback) {
-            var xhr = new XMLHttpRequest();
-            xhr.onload = function () {
-                var reader = new FileReader();
-                reader.onloadend = function () {
-                    callback(reader.result);
-                };
-                reader.readAsDataURL(xhr.response);
-            };
-            xhr.open('GET', url);
-            xhr.responseType = 'blob';
-            xhr.send();
-        },
-
-        _addData: function (data) {
-            var that = this,
-                oViewContext = this.getView().getBindingContext().getObject(),
-                oBindingObject = this.byId("idBOQUploader").getObjectBinding("DocumentUploadModel");
-            
-            data= data.substr(21, data.length);   
-
-            //set the parameters
-            oBindingObject.getParameterContext().setProperty("file", data);
-            oBindingObject.getParameterContext().setProperty("po_number", oViewContext.po_number);
-            oBindingObject.getParameterContext().setProperty("purchase_order_ID", oViewContext.ID);
-
-            //execute the action
-            oBindingObject.execute().then(
-                function () {
-                    MessageToast.show("BOQ uploaded!");
-                    that.getView().getModel().refresh();
-                },
-                function (oError) {
-                    sap.m.MessageBox.alert(oError.message, {
-                        title: "Error"
-                    });
-                }
-            );
         }
 
     });
