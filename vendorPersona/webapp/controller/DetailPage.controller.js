34a6f681314b02b57081100ab9a1cff4eac08383
Amit <amitkumar.jagadale@extentia.com>
2021-03-24 10:06:24 +0000

    FiltersAdded IconTab Added

--- vendorPersona/webapp/controller/DetailPage.controller.js
+++ vendorPersona/webapp/controller/DetailPage.controller.js
@@ -22,6 +22,29 @@ sap.ui.define([
             //Router Object
             this.oRouter = this.getRouter();
             this.oRouter.getRoute("RouteDetailPage").attachPatternMatched(this._onObjectMatched, this);
+
+            this._initializeCreationModel();
+
+            //adding searchfield association to filterbar                                    
+            this._addSearchFieldAssociationToFB();
+        },
+
+        _addSearchFieldAssociationToFB: function () {
+            let oFilterBar = this.getView().byId("filterbar");
+            let oSearchField = oFilterBar.getBasicSearch();
+            var oBasicSearch;
+            if (!oSearchField) {
+                // @ts-ignore   
+                oBasicSearch = new sap.m.SearchField({ id: "idSearch", showSearchButton: false });
+            } else {
+                oSearchField = null;
+            }
+            oFilterBar.setBasicSearch(oBasicSearch);
+            oBasicSearch.attachBrowserEvent("keyup", function (e) {
+                if (e.which === 13) {
+                    this.onSearch();
+                }
+            }.bind(this));
         },
 
         _onObjectMatched: function (oEvent) {
@@ -29,23 +52,30 @@ sap.ui.define([
             this._bindView("/Vendors" + sObjectId);
         },
 
+        _initializeCreationModel: function () {
+            var oCreationModel = new JSONModel({
+                "invoice_number": "",
+                "po_number": "",
+                "po_release_date": ""
+            });
+            this.getView().setModel(oCreationModel, "creationModel")
+        },
+
         _bindView: function (sObjectPath) {
             var objectViewModel = this.getViewModel("objectViewModel");
+             var userInfo = sap.ushell.Container.getService("UserInfo");
+             var userEmail = userInfo.getEmail();
 
-            this.getView().bindElement({
-                path: sObjectPath,
+            userEmail = userEmail || "symantic.engineering@testemail.com"
+            this.getView().byId("idPurchaseOrdersTable").bindElement({
+                path: "/Vendors",
                 parameters: {
+                    "$filter":"email eq "+userEmail,
                     "$expand": "purchase_orders"
-                },
-                events: {
-                    dataRequested: function () {
-                        objectViewModel.setProperty("/busy", true);
-                    },
-                    dataReceived: function () {
-                        objectViewModel.setProperty("/busy", false);
-                    }
                 }
             });
+
+            
         },
 
         onPurchaseOrderTableUpdateFinished: function (oEvent) {
@@ -75,7 +105,45 @@ sap.ui.define([
         //when the breadcrum pressed
         handleToAllVendorsBreadcrumPress: function (oEvent) {
             this.getRouter().navTo("RouteLandingPage");
-        }
+        },
+
+        // on Go Search 
+        onSearch: function (oEvent) {
+            var poNumber = this.byId("idNameInput").getValue();
+            var releaseDate = this.byId("DP1").getValue();
+            var aFilters = [];
+            if (poNumber != "") {
+                aFilters.push(new sap.ui.model.Filter("po_number", sap.ui.model.FilterOperator.EQ, poNumber));
+            }
+
+            if (releaseDate != "") {
+                var arr = releaseDate.split('.')
+                releaseDate = arr[2]+'-'+arr[1]+'-'+arr[0]+'T00:00:00Z'
+                aFilters.push(new sap.ui.model.Filter("po_release_date", sap.ui.model.FilterOperator.EQ, releaseDate));
+            }
+
+            var mFilters = new sap.ui.model.Filter({
+                filters: aFilters,
+                and: true
+            });
+            
+            var oTableBinding = this.getView().byId("idPurchaseOrdersTable").getBinding("items");
+            oTableBinding.filter(mFilters);
+        },
+
+        // on Date Change
+        onDateChange: function (oEvent) {
+          var   oDP = oEvent.getSource(),
+                sValue = oEvent.getParameter("value"),
+                bValid = oEvent.getParameter("valid");
+
+            if (bValid) {
+                oDP.setValueState(sap.ui.core.ValueState.None);
+            } else {
+                oDP.setValueState(sap.ui.core.ValueState.Error);
+            }
+        },
+
 
     });
 });
