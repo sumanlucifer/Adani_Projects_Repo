a254818c597adb27f546354c9e73a5e6232c6857
Venkatesh Hulekal <Venkatesh.Hulekal@extentia.com>
2021-03-29 15:07:31 +0000

    added all modules for store-inCharge

---
+++ storeInCharge-Inventory/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/agel/mmts/storeInCharge-Inventory/test/unit/controller/LandingPage.controller"
+], function () {
+	"use strict";
+});
---
+++ storeInCharge-UpcomingOrders/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/agel/mmts/storeInCharge-UpcomingOrders/test/unit/controller/LandingPage.controller"
+], function () {
+	"use strict";
+});
