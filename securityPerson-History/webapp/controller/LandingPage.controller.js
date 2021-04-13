fce922e8b5ec7f5de7555b076dcd651fcd9e35bc
Venkatesh <venkatesh.hulekal@extentia.com>
2021-04-13 04:59:35 +0000

    added histry page for security

--- securityPerson-History/webapp/controller/LandingPage.controller.js
+++ securityPerson-History/webapp/controller/LandingPage.controller.js
@@ -1,15 +1,46 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
-	/**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-	function (Controller) {
-		"use strict";
+    "./BaseController",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/core/Fragment",
+    "sap/ui/model/Sorter",
+    "sap/ui/Device"
+], function (BaseController, JSONModel, Filter, FilterOperator, Fragment, Sorter, Device) {
+    "use strict";
 
-		return Controller.extend("com.agel.mmts.securityPerson-History.controller.LandingPage", {
-			onInit: function () {
+    return BaseController.extend("com.agel.mmts.securityPerson-History.controller.LandingPage", {
+        onInit: function () {
+            //adding searchfield association to filterbar                                    
+            this._addSearchFieldAssociationToFB();
 
-			}
-		});
-	});
+            var oModel = new JSONModel({
+                email: "rakeshpatil28@gmail.com"
+            });
+            this.setModel(oModel, "oModel");
+        },
+
+        _addSearchFieldAssociationToFB: function () {
+            let oFilterBar = this.getView().byId("filterbar");
+            let oSearchField = oFilterBar.getBasicSearch();
+            var oBasicSearch;
+            if (!oSearchField) {
+                // @ts-ignore   
+                oBasicSearch = new sap.m.SearchField({ id: "idSearch", showSearchButton: false });
+            } else {
+                oSearchField = null;
+            }
+            oFilterBar.setBasicSearch(oBasicSearch);
+            oBasicSearch.attachBrowserEvent("keyup", function (e) {
+                if (e.which === 13) {
+                    this.onSearch();
+                }
+            }.bind(this));
+        },
+
+        onScannedHistoryTableUpdateStarted: function(oEvent){
+            
+        }
+
+    });
+});
