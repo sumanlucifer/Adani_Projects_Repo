8424564f7e37b52b43c8fd81c8056474ab0dba45
Mukesh Gupta <mukesh.gupta@extentia.com>
2021-06-03 06:00:25 +0000

    user_role

--- user-roleassignment/webapp/controller/UserList.controller.js
+++ user-roleassignment/webapp/controller/UserList.controller.js
@@ -46,33 +46,28 @@ sap.ui.define([
                 var that = this;
                 var sObjectPath = oItem.getBindingContext().sPath;
                 that.getRouter().navTo("RouteUserRoleDetailPage", {
-                    BOQRequestId: sObjectPath.slice("/MasterRoleSet".length)
+                    BOQRequestId: sObjectPath.slice("/UserSet".length)
                 });
             },
 
             // on Go Search 
             onSearch: function (oEvent) {
-                var usrName = this.byId("idUserName").getValue();
                 var frstName = this.byId("idfrstName").getValue();
                 var lstName = this.byId("idlstName").getValue();
                 var email = this.byId("idEmail").getValue();
+                var DateRange = this.byId("dateRangeSelectionId");
+                var DateRangeValue = this.byId("dateRangeSelectionId").getValue();
 
                 var orFilters = [];
                 var andFilters = [];
 
                 var FreeTextSearch = this.byId("filterbar").getBasicSearchValue();
                 if (FreeTextSearch) {
-                    orFilters.push(new Filter("Username", FilterOperator.Contains, FreeTextSearch));
                     orFilters.push(new Filter("FirstName", FilterOperator.Contains, FreeTextSearch));
                     orFilters.push(new Filter("LastName", FilterOperator.EQ, FreeTextSearch));
                     orFilters.push(new Filter("Email", FilterOperator.EQ, FreeTextSearch));
 
                     andFilters.push(new Filter(orFilters, false));
-                }
-
-                // UserName
-                if (usrName != "") {
-                    andFilters.push(new Filter("Username", FilterOperator.EQ, usrName));
                 }
 
                 // First Name
@@ -90,10 +85,17 @@ sap.ui.define([
                     andFilters.push(new Filter("Email", FilterOperator.EQ, email));
                 }
 
+                // Created At
+                if (DateRangeValue != "") {
+                    var From = new Date(DateRange.getFrom());
+                    var To = new Date(DateRange.getTo());
+                    andFilters.push(new Filter("CreatedAt", FilterOperator.BT, From.toISOString(), To.toISOString()));
+                }
+
 
                 var idBOQRequestTableBinding = this.getView().byId("idUserRoleTable").getTable().getBinding("items");
                 if (andFilters.length == 0) {
-                    andFilters.push(new Filter("UserName", FilterOperator.NE, ""));
+                    andFilters.push(new Filter("FirstName", FilterOperator.NE, ""));
                     idBOQRequestTableBinding.filter(new Filter(andFilters, true));
                 }
 
@@ -105,11 +107,11 @@ sap.ui.define([
             },
 
             onResetFilters: function (oEvent) {
-                //   this.byId("filterbar").setBasicSearchValue("");
-                this.byId("idUserName").setValue("");
+                this.oFilterBar._oBasicSearchField.setValue("");
                 this.byId("idfrstName").setValue("");
                 this.byId("idlstName").setValue("");
                 this.byId("idEmail").setValue("");
+                this.byId("dateRangeSelectionId").setValue("");
 
                 var oTable = this.getView().byId("idUserRoleTable").getTable();
                 var oBinding = oTable.getBinding("items");
