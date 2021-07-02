386bfa3862e0b588a218e92a68562e2bbcab13e7
Mukesh Gupta <mukesh.gupta@extentia.com>
2021-07-02 12:49:21 +0000

    security

---
+++ security-scanqr/webapp/view/blocks/packingListDetails/lineItemsTable/LineItemsTableController.controller.js
@@ -0,0 +1,26 @@
+
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.agel.mmts.securityscanqr.view.blocks.packingListDetails.packagingListContains.LineItemsTable", {
+
+        onViewBOQItemsPress: function (oEvent) {
+            this.oParentBlock.fireOnViewBOQItemsPress(oEvent);
+        },
+        onViewQRCode: function (oEvent) {
+            // this.oParentBlock.fireOnViewQRCode(oEvent);
+            this.oRouter = this.getOwnerComponent().getRouter();
+          this.oRouter.navTo("RouteQRCodeDetails");  
+        },
+        onViewQRCode1: function (oEvent) {
+            this.oParentBlock.fireOnViewQRCode1(oEvent);
+        },
+
+        //venkatesh
+        onViewQRPress: function(oEvent){
+            this.oParentBlock.fireOnViewQRCodePress(oEvent);
+        }
+      
+
+    });
+});
