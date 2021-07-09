97f04db07db08b5036b200a325f8dc3e82efdb01
Mukesh Gupta <mukesh.gupta@extentia.com>
2021-07-09 06:13:44 +0000

    merge develop

---
+++ quickAccess-Reports/dist/Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/core/UIComponent","sap/ui/Device","com/agel/mmts/quickAccess-Reports/model/models"],function(e,t,i){"use strict";return e.extend("com.agel.mmts.quickAccess-Reports.Component",{metadata:{manifest:"json"},init:function(){e.prototype.init.apply(this,arguments);this.getRouter().initialize();this.setModel(i.createDeviceModel(),"device")}})});
