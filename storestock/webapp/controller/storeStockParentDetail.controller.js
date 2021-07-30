b0b070515d0f5bb4d844bcfd2b3ba7c4c57cafb0
SUMAN SHANMUGAM <suman.shanmugam@extentia.com>
2021-07-30 15:49:21 +0000

    final changes of storestock

--- storestock/webapp/controller/storeStockParentDetail.controller.js
+++ storestock/webapp/controller/storeStockParentDetail.controller.js
@@ -16,19 +16,39 @@ sap.ui.define([
 
             onInit: function () {
                 this.oRouter = this.getRouter();
-                this.initializeFilterBar();
                 var oModel = this.getOwnerComponent().getModel("layoutModel");
                 oModel.setProperty("/layout", "OneColumn");
 
+                var oPlantModel = new JSONModel({
+                    PlntItems: []
+                });
+                this.setModel(oPlantModel, "PlantModel");
+
+                var mockPlantData = [{
+                        key: "4500327800",
+                        value: "AG-234"
+                      
+
+                    },
+                {
+                        key: "4500327801",
+                        value: "AG-34"
+                      
+
+                    },
+                {
+                        key: "4600327832",
+                        value: "AG-67"
+                      
+
+                    }
+
+            ];
 
 
-
-
-
+            this.getViewModel("PlantModel").setProperty("/PlntItems", mockPlantData);
 
             },
-
-
 
             _onObjectMatched: function (oEvent) {
                 var startupParams = this.getOwnerComponent().getComponentData().startupParameters;
@@ -43,13 +63,13 @@ sap.ui.define([
 
             onBeforeRebindUnrestTable: function (oEvent) {
                 var mBindingParams = oEvent.getParameter("bindingParams");
-                mBindingParams.filters.push(new Filter("IsRestricted", sap.ui.model.FilterOperator.EQ, true));
-                //   mBindingParams.filters.push(new Filter("PlantCode ", sap.ui.model.FilterOperator.EQ, "1001"));
+                mBindingParams.filters.push(new Filter("IsRestricted", sap.ui.model.FilterOperator.EQ, false));
+
 
             },
             onBeforeRebindRestTable: function (oEvent) {
                 var mBindingParams = oEvent.getParameter("bindingParams");
-                mBindingParams.filters.push(new Filter("IsRestricted", sap.ui.model.FilterOperator.EQ, false));
+                mBindingParams.filters.push(new Filter("IsRestricted", sap.ui.model.FilterOperator.EQ, true));
 
             },
 
@@ -57,77 +77,56 @@ sap.ui.define([
             onSearch: function (oEvent) {
 
 
-                this.byId("idParentUnRestrictedTable").mProperties.enableAutoBinding = true;
 
-                this.byId("idParentUnRestrictedTable").rebindTable();
-                // var MaterialCode = this.byId("idMaterialCode").getValue();
-                // var PlantCode = this.byId("idSelPlant").getValue();
 
-                // var orFilters = [];
-                // var andFilters = [];
+                var MaterialName = this.byId("idParentMaterialName").getValue();
+                var MaterialCode = this.byId("idParentMaterialCode").getValue();
+                var PlantCode = this.byId("idSelPlant").getSelectedKey();
+                // var PlantCode = "4500327800";
 
-                // if (PlantCode != "") {
-                //     andFilters.push(new Filter("PlantCode", FilterOperator.EQ, PlantCode));
-                //     this.byId("idParentUnRestrictedTable").mProperties.enableAutoBinding = true;
+                var orFilters = [];
+                var andFilters = [];
 
-                //     this.byId("idParentUnRestrictedTable").rebindTable();
-                // }
-                // else {
+                if (PlantCode !== "") {
 
-                //     return false;
-                // }
+                    this.byId("idParentUnRestrictedTable").mProperties.enableAutoBinding = true;
+                    this.byId("idParentUnRestrictedTable").rebindTable();
+                    this.byId("idParentRestrictedTable").mProperties.enableAutoBinding = true;
+                    this.byId("idParentRestrictedTable").rebindTable();
+                    orFilters.push(new Filter("PlantCode", FilterOperator.EQ, PlantCode));
+                }
+                else {
+                    this.byId("unrestrictedTable").setNoDataText(this.getResourceBundle().getText("EnterPlant"));
+                    this.byId("restrictedTable").setNoDataText(this.getResourceBundle().getText("EnterPlant"));
+                    return false;
+                }
 
-                // var FreeTextSearch = this.byId("filterbar").getBasicSearchValue();
-                // if (FreeTextSearch) {
-                //     orFilters.push(new Filter("PONumber", FilterOperator.Contains, FreeTextSearch));
-                //     orFilters.push(new Filter("Buyer/CompanyCode", FilterOperator.EQ, FreeTextSearch));
-                //     orFilters.push(new Filter("ParentLineItems/MaterialCode", FilterOperator.EQ, FreeTextSearch));
-                //     orFilters.push(new Filter("PlantCode", FilterOperator.EQ, FreeTextSearch));
-                //     orFilters.push(new Filter("ParentLineItems/Name", FilterOperator.Contains, FreeTextSearch));
-                //     andFilters.push(new Filter(orFilters, false));
-                // }
+                if (MaterialName != "") {
+                    orFilters.push(new Filter("Name", FilterOperator.EQ, MaterialName));
+                }
+                if (MaterialCode != "") {
+                    orFilters.push(new Filter("MaterialCode", FilterOperator.EQ, MaterialCode));
+                }
 
-                // if (poNumber != "") {
-                //     andFilters.push(new Filter("PONumber", FilterOperator.EQ, poNumber));
-                // }
+                andFilters.push(new Filter(orFilters, false));
 
-                // if (DateRangeValue != "") {
-                //     var From = new Date(DateRange.getFrom());
-                //     var To = new Date(DateRange.getTo());
-                //     andFilters.push(new Filter("POReleaseDate", FilterOperator.BT, From.toISOString(), To.toISOString()));
-                // }
 
-                // if (CompanyCode != "") {
-                //     andFilters.push(new Filter("Buyer/CompanyCode", FilterOperator.EQ, CompanyCode));
-                // }
-
-                // if (MaterialCode != "") {
-                //     andFilters.push(new Filter("ParentLineItems/MaterialCode", FilterOperator.EQ, MaterialCode));
-                // }
+                var idUnRestrictedTableBinding = this.getView().byId("idParentUnRestrictedTable").getTable().getBinding("items");
+                var idRestrictedTableBinding = this.getView().byId("idParentRestrictedTable").getTable().getBinding("items");
 
 
 
-                // var idOpenPOTableBinding = this.getView().byId("idPurchaseOrdersTable").getTable().getBinding("items");
-                // var idConfirmPOTableBinding = this.getView().byId("idConfirmPOTable").getTable().getBinding("items");
-                // var idDispatchedPOTableBinding = this.getView().byId("idDispatchedPOTable").getTable().getBinding("items");
+                if (andFilters.length > 0) {
+                    idUnRestrictedTableBinding.filter(new Filter(andFilters, true));
+                    idRestrictedTableBinding.filter(new Filter(andFilters, true));
+                }
 
-                // if (andFilters.length == 0) {
-                //     andFilters.push(new Filter("PONumber", FilterOperator.NE, ""));
-                //     idOpenPOTableBinding.filter(new Filter(andFilters, true));
-                //     idConfirmPOTableBinding.filter(new Filter(andFilters, true));
-                //     idDispatchedPOTableBinding.filter(new Filter(andFilters, true));
-                // }
 
-                // if (andFilters.length > 0) {
-                //     idOpenPOTableBinding.filter(new Filter(andFilters, true));
-                //     idConfirmPOTableBinding.filter(new Filter(andFilters, true));
-                //     idDispatchedPOTableBinding.filter(new Filter(andFilters, true));
-                // }
-                // oTableBinding.filter(mFilters);
+
             },
 
 
-            //triggers on press of a PO cheveron item from the list
+            //triggers on press of a Stock cheveron item from the list
             onRestrictdParentTablePress: function (oEvent) {
                 // The source is the list item that got pressed
                 this._showObject(oEvent.getSource());
@@ -140,7 +139,7 @@ sap.ui.define([
                 var sObjectPath = oItem.getBindingContext().sPath;
 
                 this.oRouter.navTo("storeStockChildDetail", {
-                    parentMaterial: sObjectPath.slice("/StockParentItemSet".length),// /PurchaseOrders(123)->(123)
+                    parentMaterial: sObjectPath.slice("/StockParentItemSet".length),// /StockParentItemSet(123)->(123)
                     layout: "TwoColumnsMidExpanded"
                 },
 
