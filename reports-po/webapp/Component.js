69d3afa0c0ecf3af525897ff18548db682e01fde
Venkatesh <venkatesh.hulekal@extentia.com>
2021-08-05 18:09:06 +0000

    Initial App Desogned

--- reports-po/webapp/Component.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
-    return AppComponent.extend("com.agel.mmts.reportspo.Component", {
-        metadata: {
-            manifest: "json"
-        }
-    });
-});
