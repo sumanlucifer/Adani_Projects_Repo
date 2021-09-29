17c6ac9bb7b0f6474ef5d035b89c664caca0cd57
Mukesh Gupta <mukesh.gupta@extentia.com>
2021-09-29 08:23:57 +0000

    Build review changes

--- storeincharge-returnmaterial/webapp/controller/ConfirmPage.controller.js
+++ storeincharge-returnmaterial/webapp/controller/ConfirmPage.controller.js
@@ -83,12 +83,23 @@ sap.ui.define([
             oModel.create("/ReturnMaterialPostingEdmSet", { "ID": this.ReturnId }, {
                 success: function (oRes) {
                     if (oRes.Success) {
-                        sap.m.MessageBox.success("Return request action is submitted successfully.");
-                        this.getOwnerComponent().getModel().refresh();
-                        this.getOwnerComponent().getRouter().navTo("RouteApp");
+                        MessageBox.success("Return request action is submitted successfully.", {
+                            title: "Success",
+                            onClose: function (oAction1) {
+                                if (oAction1 === sap.m.MessageBox.Action.OK) {
+                                    this.getOwnerComponent().getModel().refresh();
+                                    this.getOwnerComponent().getRouter().navTo("RouteApp");
+                                }
+                            }.bind(this)
+                        });
+                    }
+                    else {
+                        MessageBox.error("Error while submitting return request.");
                     }
                 }.bind(this),
-                error: (oErr) => { debugger }
+                error: (oErr) => {
+                    MessageBox.error("Error while submitting return request.");
+                }
             })
         }
 
