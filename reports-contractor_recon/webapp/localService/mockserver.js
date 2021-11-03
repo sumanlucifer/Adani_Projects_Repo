a91249fd362a2c0e3941171d197b70fcb5ccb319
Mukesh Gupta <mukesh.gupta@extentia.com>
2021-11-03 08:14:26 +0000

    report contractor Recon

--- reports-contractor_recon/webapp/localService/mockserver.js
+++ reports-contractor_recon/webapp/localService/mockserver.js
@@ -1,7 +1,7 @@
 sap.ui.define(['sap/ui/core/util/MockServer'], function(MockServer) {
     'use strict';
     var oMockServer,
-        _sAppModulePath = 'com/agel/mmts/reportscontractorreconciliation/',
+        _sAppModulePath = 'com/agel/mmts/reportscontractorrecon/',
         _sJsonFilesModulePath = _sAppModulePath + 'localService/mockdata';
 
     return {
