848dea23406f372b2d2b7bcdb8a589ef9bd241a9
Mukesh Gupta <mukesh.gupta@extentia.com>
2021-12-22 10:15:09 +0000

    workflow changes

--- workflow-PMCGApprovals/scripts/PMCGApprovals/setApprovalStatus.js
+++ workflow-PMCGApprovals/scripts/PMCGApprovals/setApprovalStatus.js
@@ -1,6 +1,6 @@
 var userTaskDecision = $.usertasks.usertask1.last.decision;
 
-if(userTaskDecision.toLowerCase() === 'approve')
+if(userTaskDecision.toLowerCase() === 'approved')
     $.context.Status = "Approved";
 else
     $.context.Status = "Rejected";
